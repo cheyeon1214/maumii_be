2450f0c7d44d380eb0946698769e56e6887b2b0f
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-25 20:59:49 +0900

    feat: pyhon 감정 분석 관련 코드

--- src/main/java/com/project/maumii_be/MaumiiBeApplication.java
+++ src/main/java/com/project/maumii_be/MaumiiBeApplication.java
@@ -1,13 +1,30 @@
 package com.project.maumii_be;
 
+import com.project.maumii_be.dto.EmotionResponse;
+import com.project.maumii_be.service.EmotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MaumiiBeApplication {
+public class MaumiiBeApplication implements CommandLineRunner {
+
+	@Autowired
+	private EmotionService emotionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaumiiBeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		// FastAPI 테스트
+//		String text = "오늘 기분이 너무 좋아!";
+//		EmotionResponse response = emotionService.analyzeText(text);
+//
+//		System.out.println("=== FastAPI 테스트 결과 ===");
+//		System.out.println("Label: " + response.getLabel());
+//		System.out.println("Score: " + response.getScore());
+	}
 }
---
+++ src/main/java/com/project/maumii_be/service/EmotionService.java
@@ -0,0 +1,26 @@
+package com.project.maumii_be.service;
+
+import com.project.maumii_be.dto.EmotionResponse;
+import com.project.maumii_be.dto.TextRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EmotionService {
+
+    private final RestTemplate restTemplate;
+
+    public EmotionService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public EmotionResponse analyzeText(String text) {
+        TextRequest request = new TextRequest();
+        request.setText(text);
+
+        // POST 로 /analyze 호출
+        EmotionResponse response = restTemplate.postForObject("/analyze", request, EmotionResponse.class);
+
+        return response;
+    }
+}
