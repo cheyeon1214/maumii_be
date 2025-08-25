5faa01d386f753f2ecdaa825762abb8b6d951e64
cheny <cheyeon001214@naver.com>
2025-08-25 18:06:06 +0900

    first commit

---
+++ src/test/java/com/project/maumii_be/MaumiiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.maumii_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaumiiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
