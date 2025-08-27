325d88a7edc3d57c6dbcacbc65af57fb495ae7f1
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-27 12:36:40 +0900

    feat: record 관련 repository 작성

--- src/main/java/com/project/maumii_be/repository/BubbleRepository.java
+++ src/main/java/com/project/maumii_be/repository/BubbleRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BubbleRepository extends JpaRepository<Bubble, Long> {
-    @Query(value = "select b from Bubble b join fetch b.record r join fetch r.recordList rl where rl.rlId = :rlId order by r.rId desc")
+
+    // 녹음 리스트 상세 조회
+    @Query(value = "select b from Bubble b join fetch b.record r join fetch r.recordList rl where rl.rlId = :rlId order by r.rId desc, b.bId asc")
     List<Bubble> getRecord(Long rlId);
+
+    // 버블 정보 조회
+    Optional<Bubble> findById(Long bId);
+
+    // 버블 텍스트 및 감정 수정 ... update
 }
