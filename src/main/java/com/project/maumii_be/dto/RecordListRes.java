399837c0ad93fbaf3a4709f319cd5c554d0b3c35
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-27 19:04:49 +0900

    feat: record 부분 controller 작성

--- src/main/java/com/project/maumii_be/dto/RecordListRes.java
+++ src/main/java/com/project/maumii_be/dto/RecordListRes.java
@@ -3,6 +3,8 @@ package com.project.maumii_be.dto;
 import com.project.maumii_be.domain.RecordList;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -11,12 +13,14 @@ import lombok.*;
 public class RecordListRes {
     Long rlId;
     String rlName;
+    LocalDateTime updateDate;
     String uId;
 
     public RecordListRes toRecordListRes(RecordList recordList) {
         return RecordListRes.builder()
                 .rlId(recordList.getRlId())
                 .rlName(recordList.getRlName())
+                .updateDate(recordList.getUpdateDate())
                 .uId(recordList.getUser().getUId())
                 .build();
     }
