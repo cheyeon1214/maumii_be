7dfb2e25695dbd6fb5b52bf82a8816e98c4f1ba1
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-28 17:28:30 +0900

    feat: record list controller 연결 중

--- src/main/java/com/project/maumii_be/repository/RecordListRepository.java
+++ src/main/java/com/project/maumii_be/repository/RecordListRepository.java
@@ -12,6 +12,12 @@ public interface RecordListRepository extends JpaRepository<RecordList, Long> {
     @Query(value = "select rl from RecordList rl join fetch rl.user u where u.uId = :userId order by rl.updateDate desc")
     List<RecordList> findByUser_uIdOrderByRlIdDesc(String userId);
 
+    // 날짜에 해당하는 레코드 리스트 조회
+
+    // 단어로 검색하여 레코드 리스트 조회
+    @Query(value="select rl from RecordList rl join fetch rl.user u where u.uId = :userId and rl.rlName like :keyword order by rl.updateDate desc")
+    List<RecordList> findByRlNameLike(String userId, String keyword);
+
     // 녹음 리스트 삭제
     // void deleteById(Long rlId);
 
